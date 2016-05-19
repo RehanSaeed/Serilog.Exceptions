ec36ab0096d3a821cec0fbb30fd7b6fa976b2ebd
Muhammad Rehan Saeed <rehansaeed@gmail.com>
2016-05-19 10:34:54 +0100

    Initial checkin

--- Source/Serilog.Exceptions/Properties/AssemblyInfo.cs
+++ Source/Serilog.Exceptions/Properties/AssemblyInfo.cs
@@ -1,23 +1,13 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+﻿using System;
+using System.Reflection;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("Serilog.Exceptions")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Serilog.Exceptions is an add-on to Serilog (http://serilog.net/) to log exception details and custom properties that are not output in Exception.ToString().")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Serilog.Exceptions")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright © Muhammad Rehan Saeed 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("d8034654-4a6f-4857-9637-62098fc9267a")]
+[assembly: CLSCompliant(true)]
