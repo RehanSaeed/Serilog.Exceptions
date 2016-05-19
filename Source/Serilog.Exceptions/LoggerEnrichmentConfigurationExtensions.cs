6f177e1074e13e85a26cfa3a3f0db3f9e3c8fde0
Muhammad Rehan Saeed <rehansaeed@gmail.com>
2016-05-19 10:40:16 +0100

    Added WithLazyProperties enricher

--- Source/Serilog.Exceptions/LoggerEnrichmentConfigurationExtensions.cs
+++ Source/Serilog.Exceptions/LoggerEnrichmentConfigurationExtensions.cs
@@ -55,5 +55,34 @@
 
             return loggerConfiguration;
         }
+
+        public static Serilog.LoggerConfiguration WithLazyProperties(
+            this LoggerEnrichmentConfiguration loggerEnrichmentConfiguration,
+            IDictionary<string, Func<object>> properties)
+        {
+            if (loggerEnrichmentConfiguration == null)
+            {
+                throw new ArgumentNullException(nameof(loggerEnrichmentConfiguration));
+            }
+
+            if (properties == null)
+            {
+                throw new ArgumentNullException(nameof(properties));
+            }
+
+            if (properties.Count == 0)
+            {
+                throw new ArgumentException("Logger properties count cannot be zero.", nameof(properties));
+            }
+
+            Serilog.LoggerConfiguration loggerConfiguration = null;
+
+            foreach (var property in properties)
+            {
+                loggerConfiguration = loggerEnrichmentConfiguration.WithProperty(property.Key, property.Value());
+            }
+
+            return loggerConfiguration;
+        }
     }
 }
