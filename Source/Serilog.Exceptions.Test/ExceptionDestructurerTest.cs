968bc8c57b49a82d35179e109a9a197abfb3c5e2
Jérémie Bertrand <jeremie.bertrand@gmail.com>
2016-08-31 19:42:43 +0200

    Fix TypeLoadException on mono
    Fix #2

---
+++ Source/Serilog.Exceptions.Test/ExceptionDestructurerTest.cs
@@ -0,0 +1,39 @@
+﻿namespace Serilog.Exceptions.Test
+{
+    using Destructurers;
+    using System;
+    using Xunit;
+
+    public class ExceptionDestructurerTest
+    {
+        [Fact]
+        public void TargetTypes()
+        {
+            // arrange
+            var destructurer = new ExceptionDestructurer();
+
+            // act
+            var targetTypes = destructurer.TargetTypes;
+
+            // assert
+            if (Type.GetType("System.Diagnostics.Eventing.Reader.EventLogInvalidDataException, System.Core, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089") != null)
+            {
+                // full .net
+                Assert.Equal(128, targetTypes.Length);
+                Assert.Contains(targetTypes, t => t.FullName == "System.Diagnostics.Eventing.Reader.EventLogInvalidDataException");
+                Assert.Contains(targetTypes, t => t.FullName == "System.Diagnostics.Eventing.Reader.EventLogNotFoundException");
+                Assert.Contains(targetTypes, t => t.FullName == "System.Diagnostics.Eventing.Reader.EventLogProviderDisabledException");
+                Assert.Contains(targetTypes, t => t.FullName == "System.Diagnostics.Eventing.Reader.EventLogReadingException");
+                Assert.Contains(targetTypes, t => t.FullName == "System.Management.Instrumentation.InstanceNotFoundException");
+                Assert.Contains(targetTypes, t => t.FullName == "System.Management.Instrumentation.InstrumentationBaseException");
+                Assert.Contains(targetTypes, t => t.FullName == "System.Management.Instrumentation.InstrumentationException");
+            }
+            else
+            {
+                // mono
+                Assert.Equal(121, targetTypes.Length);
+            }
+
+        }
+    }
+}
--- Source/Serilog.Exceptions/Destructurers/ExceptionDestructurer.cs
+++ Source/Serilog.Exceptions/Destructurers/ExceptionDestructurer.cs
@@ -11,7 +11,7 @@
         {
             get
             {
-                return new Type[]
+                var targetTypes = new List<Type>
                     {
                         typeof(Microsoft.CSharp.RuntimeBinder.RuntimeBinderException),
                         typeof(Microsoft.CSharp.RuntimeBinder.RuntimeBinderInternalCompilerException),
@@ -63,12 +63,6 @@
                         typeof(System.Data.VersionNotFoundException),
 #endif
                         typeof(System.DataMisalignedException),
-#if NET45
-                        typeof(System.Diagnostics.Eventing.Reader.EventLogInvalidDataException),
-                        typeof(System.Diagnostics.Eventing.Reader.EventLogNotFoundException),
-                        typeof(System.Diagnostics.Eventing.Reader.EventLogProviderDisabledException),
-                        typeof(System.Diagnostics.Eventing.Reader.EventLogReadingException),
-#endif
 
                         typeof(System.Diagnostics.Tracing.EventSourceException),
                         typeof(System.DivideByZeroException),
@@ -184,7 +178,33 @@
                         typeof(System.UnauthorizedAccessException),
                         typeof(System.UriFormatException)
                     };
+
+#if NET45
+                foreach (var dangerousType in GetDangerousTypes())
+                {
+                    var type = Type.GetType(dangerousType);
+                    if(type != null)
+                    {
+                        targetTypes.Add(type);
+                    }
+                }
+#endif
+                return targetTypes.ToArray();
             }
+        }
+
+        private string[] GetDangerousTypes()
+        {
+            return new[]
+            {
+                "System.Diagnostics.Eventing.Reader.EventLogInvalidDataException, System.Core, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089",
+                "System.Diagnostics.Eventing.Reader.EventLogNotFoundException, System.Core, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089",
+                "System.Diagnostics.Eventing.Reader.EventLogProviderDisabledException, System.Core, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089",
+                "System.Diagnostics.Eventing.Reader.EventLogReadingException, System.Core, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089",
+                "System.Management.Instrumentation.InstanceNotFoundException, System.Core, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089",
+                "System.Management.Instrumentation.InstrumentationBaseException, System.Core, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089",
+                "System.Management.Instrumentation.InstrumentationException, System.Core, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089"
+            };
         }
 
         public virtual void Destructure(
