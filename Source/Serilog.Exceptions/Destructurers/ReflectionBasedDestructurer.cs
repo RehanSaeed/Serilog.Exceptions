93491e3a40ed0202c482edd9c86a49a3a4682c45
Rico Suter <mail@rsuter.com>
2016-11-17 10:42:41 +0100

    Detect cyclic loops and add $ref and $id properties

--- Source/Serilog.Exceptions/Destructurers/ReflectionBasedDestructurer.cs
+++ Source/Serilog.Exceptions/Destructurers/ReflectionBasedDestructurer.cs
@@ -20,13 +20,13 @@
             IDictionary<string, object> data,
             Func<Exception, IDictionary<string, object>> destructureException)
         {
-            foreach (var p in this.DestructureObject(exception, exception.GetType(), 0))
+            foreach (var p in this.DestructureObject(exception, exception.GetType(), 0, new Dictionary<object, IDictionary<string, object>>()))
             {
                 data.Add(p.Key, p.Value);
             }
         }
 
-        private object DestructureValue(object value, int level)
+        private object DestructureValue(object value, int level, IDictionary<object, IDictionary<string, object>> destructuredObjects)
         {
             if (value == null)
             {
@@ -56,28 +56,48 @@
                 return ((IDictionary)value)
                     .Cast<DictionaryEntry>()
                     .Where(e => e.Key is string)
-                    .ToDictionary(e => (string)e.Key, e => this.DestructureValue(e.Value, level + 1));
+                    .ToDictionary(e => (string)e.Key, e => this.DestructureValue(e.Value, level + 1, destructuredObjects));
             }
 
             if (typeof(IEnumerable).GetTypeInfo().IsAssignableFrom(valueTypeInfo))
             {
                 return ((IEnumerable)value)
                     .Cast<object>()
-                    .Select(o => this.DestructureValue(o, level + 1))
+                    .Select(o => this.DestructureValue(o, level + 1, destructuredObjects))
                     .ToList();
             }
 
-            return this.DestructureObject(value, valueType, level);
+            return this.DestructureObject(value, valueType, level, destructuredObjects);
         }
 
-        private IDictionary<string, object> DestructureObject(object value, Type valueType, int level)
+        private IDictionary<string, object> DestructureObject(object value, Type valueType, int level, IDictionary<object, IDictionary<string, object>> destructuredObjects)
         {
-            var values = valueType
+            if (destructuredObjects.ContainsKey(value))
+            {
+                var id = destructuredObjects.Keys
+                    .Where(v => v == value)
+                    .Select((v, i) => i)
+                    .Single() + 1;
+
+                destructuredObjects[value].Add("$id", id.ToString());
+
+                return new Dictionary<string, object>
+                {
+                    { "$ref", id.ToString() }
+                };
+            }
+
+            var values = new Dictionary<string, object>();
+            destructuredObjects.Add(value, values);
+
+            var properties = valueType
                 .GetProperties(BindingFlags.Public | BindingFlags.Instance)
-                .Where(x => x.CanRead)
-                .ToDictionary(
-                    x => x.Name,
-                    x => this.DestructureValue(x.GetValue(value), level + 1));
+                .Where(x => x.CanRead);
+
+            foreach (var property in properties)
+            {
+                values.Add(property.Name, this.DestructureValue(property.GetValue(value), level + 1, destructuredObjects));
+            }
 
             values.Add("Type", valueType);
 
