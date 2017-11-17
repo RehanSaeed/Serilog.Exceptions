39ec00c2a9763c4363a456546bb25a1800c790b2
Muhammad Rehan Saeed <rehan.saeed@bridgeinternationalacademies.com>
2017-11-17 09:11:41 +0000

    Stop creating list of exceptions for every instance

--- Source/Serilog.Exceptions/Destructurers/ExceptionDestructurer.cs
+++ Source/Serilog.Exceptions/Destructurers/ExceptionDestructurer.cs
@@ -1,4 +1,4 @@
-﻿namespace Serilog.Exceptions.Destructurers
+namespace Serilog.Exceptions.Destructurers
 {
     using System;
     using System.Collections;
@@ -7,184 +7,182 @@
 
     public class ExceptionDestructurer : IExceptionDestructurer
     {
-        public virtual Type[] TargetTypes
+        private static readonly List<Type> TargetTypesList = new List<Type>
         {
-            get
-            {
-                var targetTypes = new List<Type>
-                    {
-                        typeof(Microsoft.CSharp.RuntimeBinder.RuntimeBinderException),
-                        typeof(Microsoft.CSharp.RuntimeBinder.RuntimeBinderInternalCompilerException),
+            typeof(Microsoft.CSharp.RuntimeBinder.RuntimeBinderException),
+            typeof(Microsoft.CSharp.RuntimeBinder.RuntimeBinderInternalCompilerException),
 #if NET45
-                        typeof(Microsoft.SqlServer.Server.InvalidUdtException),
-                        typeof(System.AccessViolationException),
-                        typeof(System.AppDomainUnloadedException),
-                        typeof(System.ApplicationException),
+            typeof(Microsoft.SqlServer.Server.InvalidUdtException),
+            typeof(System.AccessViolationException),
+            typeof(System.AppDomainUnloadedException),
+            typeof(System.ApplicationException),
 #endif
-                        typeof(System.ArithmeticException),
-                        typeof(System.ArrayTypeMismatchException),
+            typeof(System.ArithmeticException),
+            typeof(System.ArrayTypeMismatchException),
 #if NET45
-                        typeof(System.CannotUnloadAppDomainException),
+            typeof(System.CannotUnloadAppDomainException),
 #endif
-                        typeof(System.Collections.Generic.KeyNotFoundException),
+            typeof(System.Collections.Generic.KeyNotFoundException),
 #if NET45
-                        typeof(System.ComponentModel.Design.CheckoutException),
-                        typeof(System.ComponentModel.InvalidAsynchronousStateException),
-                        typeof(System.ComponentModel.InvalidEnumArgumentException),
-                        typeof(System.Configuration.SettingsPropertyIsReadOnlyException),
-                        typeof(System.Configuration.SettingsPropertyNotFoundException),
-                        typeof(System.Configuration.SettingsPropertyWrongTypeException),
-                        typeof(System.ContextMarshalException),
+            typeof(System.ComponentModel.Design.CheckoutException),
+            typeof(System.ComponentModel.InvalidAsynchronousStateException),
+            typeof(System.ComponentModel.InvalidEnumArgumentException),
+            typeof(System.Configuration.SettingsPropertyIsReadOnlyException),
+            typeof(System.Configuration.SettingsPropertyNotFoundException),
+            typeof(System.Configuration.SettingsPropertyWrongTypeException),
+            typeof(System.ContextMarshalException),
 #endif
-                        typeof(System.Data.Common.DbException),
 #if NET45
-                        typeof(System.Data.ConstraintException),
-                        typeof(System.Data.DataException),
-                        typeof(System.Data.DeletedRowInaccessibleException),
-                        typeof(System.Data.DuplicateNameException),
-                        typeof(System.Data.EvaluateException),
-                        typeof(System.Data.InRowChangingEventException),
-                        typeof(System.Data.InvalidConstraintException),
-                        typeof(System.Data.InvalidExpressionException),
-                        typeof(System.Data.MissingPrimaryKeyException),
-                        typeof(System.Data.NoNullAllowedException),
-                        typeof(System.Data.OperationAbortedException),
-                        typeof(System.Data.ReadOnlyException),
-                        typeof(System.Data.RowNotInTableException),
-                        typeof(System.Data.SqlTypes.SqlAlreadyFilledException),
-                        typeof(System.Data.SqlTypes.SqlNotFilledException),
+            typeof(System.Data.ConstraintException),
+            typeof(System.Data.DataException),
+            typeof(System.Data.DeletedRowInaccessibleException),
+            typeof(System.Data.DuplicateNameException),
+            typeof(System.Data.EvaluateException),
+            typeof(System.Data.InRowChangingEventException),
+            typeof(System.Data.InvalidConstraintException),
+            typeof(System.Data.InvalidExpressionException),
+            typeof(System.Data.MissingPrimaryKeyException),
+            typeof(System.Data.NoNullAllowedException),
+            typeof(System.Data.OperationAbortedException),
+            typeof(System.Data.ReadOnlyException),
+            typeof(System.Data.RowNotInTableException),
+            typeof(System.Data.SqlTypes.SqlAlreadyFilledException),
+            typeof(System.Data.SqlTypes.SqlNotFilledException),
 #endif
-                        typeof(System.Data.SqlTypes.SqlNullValueException),
-                        typeof(System.Data.SqlTypes.SqlTruncateException),
-                        typeof(System.Data.SqlTypes.SqlTypeException),
 #if NET45
-                        typeof(System.Data.StrongTypingException),
-                        typeof(System.Data.SyntaxErrorException),
-                        typeof(System.Data.VersionNotFoundException),
+            typeof(System.Data.StrongTypingException),
+            typeof(System.Data.SyntaxErrorException),
+            typeof(System.Data.VersionNotFoundException),
 #endif
-                        typeof(System.DataMisalignedException),
-                        typeof(System.DivideByZeroException),
-                        typeof(System.DllNotFoundException),
+            typeof(System.DataMisalignedException),
+            typeof(System.DivideByZeroException),
+            typeof(System.DllNotFoundException),
 #if NET45
-                        typeof(System.DuplicateWaitObjectException),
-                        typeof(System.EntryPointNotFoundException),
+            typeof(System.DuplicateWaitObjectException),
+            typeof(System.EntryPointNotFoundException),
 #endif
-                        typeof(System.Exception),
-                        typeof(System.FieldAccessException),
-                        typeof(System.FormatException),
-                        typeof(System.IndexOutOfRangeException),
-                        typeof(System.InsufficientExecutionStackException),
+            typeof(System.Exception),
+            typeof(System.FieldAccessException),
+            typeof(System.FormatException),
+            typeof(System.IndexOutOfRangeException),
+            typeof(System.InsufficientExecutionStackException),
 #if NET45
-                        typeof(System.InsufficientMemoryException),
+            typeof(System.InsufficientMemoryException),
 #endif
-                        typeof(System.InvalidCastException),
-                        typeof(System.InvalidOperationException),
-                        typeof(System.InvalidProgramException),
-                        typeof(System.InvalidTimeZoneException),
-                        typeof(System.IO.DirectoryNotFoundException),
+            typeof(System.InvalidCastException),
+            typeof(System.InvalidOperationException),
+            typeof(System.InvalidProgramException),
+            typeof(System.InvalidTimeZoneException),
+            typeof(System.IO.DirectoryNotFoundException),
 #if NET45
-                        typeof(System.IO.DriveNotFoundException),
+            typeof(System.IO.DriveNotFoundException),
 #endif
-                        typeof(System.IO.EndOfStreamException),
+            typeof(System.IO.EndOfStreamException),
 #if NET45
-                        typeof(System.IO.InternalBufferOverflowException),
+            typeof(System.IO.InternalBufferOverflowException),
 #endif
-                        typeof(System.IO.InvalidDataException),
-                        typeof(System.IO.IOException),
+            typeof(System.IO.InvalidDataException),
+            typeof(System.IO.IOException),
 #if NET45
-                        typeof(System.IO.IsolatedStorage.IsolatedStorageException),
+            typeof(System.IO.IsolatedStorage.IsolatedStorageException),
 #endif
-                        typeof(System.IO.PathTooLongException),
-                        typeof(System.MemberAccessException),
-                        typeof(System.MethodAccessException),
+            typeof(System.IO.PathTooLongException),
+            typeof(System.MemberAccessException),
+            typeof(System.MethodAccessException),
 #if NET45
-                        typeof(System.MulticastNotSupportedException),
+            typeof(System.MulticastNotSupportedException),
 #endif
-                        typeof(System.Net.CookieException),
+            typeof(System.Net.CookieException),
 #if NET45
-                        typeof(System.Net.NetworkInformation.PingException),
-                        typeof(System.Net.ProtocolViolationException),
+            typeof(System.Net.NetworkInformation.PingException),
+            typeof(System.Net.ProtocolViolationException),
 #endif
-                        typeof(System.NotImplementedException),
-                        typeof(System.NotSupportedException),
-                        typeof(System.NullReferenceException),
-                        typeof(System.OutOfMemoryException),
-                        typeof(System.OverflowException),
-                        typeof(System.PlatformNotSupportedException),
-                        typeof(System.RankException),
-                        typeof(System.Reflection.AmbiguousMatchException),
+            typeof(System.NotImplementedException),
+            typeof(System.NotSupportedException),
+            typeof(System.NullReferenceException),
+            typeof(System.OutOfMemoryException),
+            typeof(System.OverflowException),
+            typeof(System.PlatformNotSupportedException),
+            typeof(System.RankException),
+            typeof(System.Reflection.AmbiguousMatchException),
 #if NET45
-                        typeof(System.Reflection.CustomAttributeFormatException),
+            typeof(System.Reflection.CustomAttributeFormatException),
 #endif
 #if !NETSTANDARD1_3
-                        typeof(System.Reflection.InvalidFilterCriteriaException),
-                        typeof(System.Reflection.TargetException),
+            typeof(System.Reflection.InvalidFilterCriteriaException),
+            typeof(System.Reflection.TargetException),
 #endif
-                        typeof(System.Reflection.TargetInvocationException),
-                        typeof(System.Reflection.TargetParameterCountException),
-                        typeof(System.Resources.MissingManifestResourceException),
-                        typeof(System.Runtime.InteropServices.COMException),
-                        typeof(System.Runtime.InteropServices.InvalidComObjectException),
-                        typeof(System.Runtime.InteropServices.InvalidOleVariantTypeException),
-                        typeof(System.Runtime.InteropServices.MarshalDirectiveException),
-                        typeof(System.Runtime.InteropServices.SafeArrayRankMismatchException),
-                        typeof(System.Runtime.InteropServices.SafeArrayTypeMismatchException),
-                        typeof(System.Runtime.InteropServices.SEHException),
+            typeof(System.Reflection.TargetInvocationException),
+            typeof(System.Reflection.TargetParameterCountException),
+            typeof(System.Resources.MissingManifestResourceException),
+            typeof(System.Runtime.InteropServices.COMException),
+            typeof(System.Runtime.InteropServices.InvalidComObjectException),
+            typeof(System.Runtime.InteropServices.InvalidOleVariantTypeException),
+            typeof(System.Runtime.InteropServices.MarshalDirectiveException),
+            typeof(System.Runtime.InteropServices.SafeArrayRankMismatchException),
+            typeof(System.Runtime.InteropServices.SafeArrayTypeMismatchException),
+            typeof(System.Runtime.InteropServices.SEHException),
 #if NET45
-                        typeof(System.Runtime.Remoting.RemotingException),
-                        typeof(System.Runtime.Remoting.RemotingTimeoutException),
-                        typeof(System.Runtime.Remoting.ServerException),
-                        typeof(System.Runtime.Serialization.SerializationException),
-                        typeof(System.Security.Authentication.AuthenticationException),
-                        typeof(System.Security.Authentication.InvalidCredentialException),
+            typeof(System.Runtime.Remoting.RemotingException),
+            typeof(System.Runtime.Remoting.RemotingTimeoutException),
+            typeof(System.Runtime.Remoting.ServerException),
+            typeof(System.Runtime.Serialization.SerializationException),
+            typeof(System.Security.Authentication.AuthenticationException),
+            typeof(System.Security.Authentication.InvalidCredentialException),
 #endif
-                        typeof(System.Security.Cryptography.CryptographicException),
+            typeof(System.Security.Cryptography.CryptographicException),
 #if NET45
-                        typeof(System.Security.Cryptography.CryptographicUnexpectedOperationException),
-                        typeof(System.Security.Policy.PolicyException),
+            typeof(System.Security.Cryptography.CryptographicUnexpectedOperationException),
+            typeof(System.Security.Policy.PolicyException),
 #endif
-                        typeof(System.Security.VerificationException),
+            typeof(System.Security.VerificationException),
 #if NET45
-                        typeof(System.Security.XmlSyntaxException),
-                        typeof(System.StackOverflowException),
-                        typeof(System.SystemException),
+            typeof(System.Security.XmlSyntaxException),
+            typeof(System.StackOverflowException),
+            typeof(System.SystemException),
 #endif
-                        typeof(System.Threading.BarrierPostPhaseException),
-                        typeof(System.Threading.LockRecursionException),
-                        typeof(System.Threading.SemaphoreFullException),
-                        typeof(System.Threading.SynchronizationLockException),
-                        typeof(System.Threading.Tasks.TaskSchedulerException),
+            typeof(System.Threading.BarrierPostPhaseException),
+            typeof(System.Threading.LockRecursionException),
+            typeof(System.Threading.SemaphoreFullException),
+            typeof(System.Threading.SynchronizationLockException),
+            typeof(System.Threading.Tasks.TaskSchedulerException),
 #if NET45
-                        typeof(System.Threading.ThreadInterruptedException),
-                        typeof(System.Threading.ThreadStartException),
-                        typeof(System.Threading.ThreadStateException),
+            typeof(System.Threading.ThreadInterruptedException),
+            typeof(System.Threading.ThreadStartException),
+            typeof(System.Threading.ThreadStateException),
 #endif
-                        typeof(System.Threading.WaitHandleCannotBeOpenedException),
-                        typeof(System.TimeoutException),
+            typeof(System.Threading.WaitHandleCannotBeOpenedException),
+            typeof(System.TimeoutException),
 #if NET45
-                        typeof(System.TimeZoneNotFoundException),
+            typeof(System.TimeZoneNotFoundException),
 #endif
-                        typeof(System.TypeAccessException),
+            typeof(System.TypeAccessException),
 #if NET45
-                        typeof(System.TypeUnloadedException),
+            typeof(System.TypeUnloadedException),
 #endif
-                        typeof(System.UnauthorizedAccessException),
-                        typeof(System.UriFormatException)
-                    };
+            typeof(System.UnauthorizedAccessException),
+            typeof(System.UriFormatException)
+        };
 
+        private static readonly Type[] TargetTypesArray;
+
+        static ExceptionDestructurer()
+        {
 #if NET45
-                foreach (var dangerousType in GetNotHandledByMonoTypes())
+            foreach (var dangerousType in GetNotHandledByMonoTypes())
+            {
+                var type = Type.GetType(dangerousType);
+                if (type != null)
                 {
-                    var type = Type.GetType(dangerousType);
-                    if (type != null)
-                    {
-                        targetTypes.Add(type);
-                    }
+                    TargetTypesList.Add(type);
                 }
-#endif
-                return targetTypes.ToArray();
             }
+#endif
+
+            TargetTypesArray = TargetTypesList.ToArray();
         }
+
+        public virtual Type[] TargetTypes { get => TargetTypesArray; }
 
         public virtual void Destructure(
             Exception exception,
