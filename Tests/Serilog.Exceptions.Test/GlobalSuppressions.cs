c775562d917749a72052298a9df49284a360009b
Artur Krajewski <krajek.dev@gmail.com>
2018-02-09 21:23:52 +0100

    Enlarge the scope of SA1600 supression to whole test project

--- Tests/Serilog.Exceptions.Test/GlobalSuppressions.cs
+++ Tests/Serilog.Exceptions.Test/GlobalSuppressions.cs
@@ -3,4 +3,4 @@
 // Project-level suppressions either have no target or are given
 // a specific target and scoped to a namespace, type, member, etc.
 
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1600:Elements should be documented", Justification = "Test project classes do not need to be documented", Scope = "type", Target = "~T:Serilog.Exceptions.Test.Destructurers.ReflectionBasedDestructurerTest")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1600:Elements should be documented", Justification = "Test project classes do not need to be documented")]
