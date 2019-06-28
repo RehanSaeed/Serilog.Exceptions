1f22ada00506c4d050be85e9179001898eeae3b2
Joel Weiss <joelweiss@outlook.com>
2019-06-28 18:43:08 -0400

    Fixed: Do not Qurery Db in DbUpdateException #100

---
+++ Source/Serilog.Exceptions.EntityFrameworkCore/Destructurers/DbUpdateExceptionDestructurer.cs
@@ -0,0 +1,45 @@
+namespace Serilog.Exceptions.SqlServer.Destructurers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.EntityFrameworkCore;
+    using Serilog.Exceptions.Core;
+    using Serilog.Exceptions.Destructurers;
+
+    /// <summary>
+    /// A destructurer for <see cref="DbUpdateException"/>.
+    /// </summary>
+    /// <seealso cref="ExceptionDestructurer" />
+    public class DbUpdateExceptionDestructurer : ExceptionDestructurer
+    {
+        /// <inheritdoc />
+        public override Type[] TargetTypes => new[] { typeof(DbUpdateException), typeof(DbUpdateConcurrencyException) };
+
+        /// <inheritdoc />
+        public override void Destructure(
+            Exception exception,
+            IExceptionPropertiesBag propertiesBag,
+            Func<Exception, IReadOnlyDictionary<string, object>> destructureException)
+        {
+            base.Destructure(exception, propertiesBag, destructureException);
+
+#pragma warning disable CA1062 // Validate arguments of public methods
+            var dbUpdateException = (DbUpdateException)exception;
+            var entriesValue = dbUpdateException.Entries?.Select(e => new
+            {
+                EntryProperties = e.Properties.Select(p => new
+                {
+                    PropertyName = p.Metadata.Name,
+                    p.OriginalValue,
+                    p.CurrentValue,
+                    p.IsTemporary,
+                    p.IsModified
+                }),
+                e.State
+            }).ToList();
+            propertiesBag.AddProperty(nameof(DbUpdateException.Entries), entriesValue);
+#pragma warning restore CA1062 // Validate arguments of public methods
+        }
+    }
+}
---
+++ Tests/Serilog.Exceptions.Test/Destructurers/DbUpdateExceptionDestructurerTest.cs
@@ -0,0 +1,115 @@
+namespace Serilog.Exceptions.Test.Destructurers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using FluentAssertions;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.ChangeTracking;
+    using Serilog.Exceptions.Core;
+    using Serilog.Exceptions.SqlServer.Destructurers;
+    using Serilog.Formatting.Json;
+    using Xunit;
+    using static LogJsonOutputUtils;
+
+    public class DbUpdateExceptionDestructurerTest
+    {
+        [Fact]
+        public void WithoutDbUpdateExceptionDestructurerShouldLogDbValues()
+        {
+            // Arrange
+            var jsonWriter = new StringWriter();
+            ILogger logger = new LoggerConfiguration()
+                .Enrich.WithExceptionDetails(new DestructuringOptionsBuilder().WithDefaultDestructurers())
+                .WriteTo.Sink(new TestTextWriterSink(jsonWriter, new JsonFormatter()))
+                .CreateLogger();
+            var ctx = new TestContext();
+            ctx.Database.EnsureDeleted();
+            ctx.Database.EnsureCreated();
+
+            var entry = ctx.Add(new User
+            {
+                UserId = Guid.NewGuid().ToString()
+            });
+
+            // Act
+            logger.Error(new TestDbUpdateException("DbUpdate Error", entry), "Error");
+
+            // Assert
+            var writtenJson = jsonWriter.ToString();
+            writtenJson.Should().Contain(TestContext.UserIdIDoNotWantToSee);
+        }
+
+        [Fact]
+        public void WithDbUpdateExceptionDestructurerShouldNotLogDbValues()
+        {
+            // Arrange
+            var jsonWriter = new StringWriter();
+            ILogger logger = new LoggerConfiguration()
+                .Enrich.WithExceptionDetails(new DestructuringOptionsBuilder().WithDefaultDestructurers().WithDestructurers(new[] { new TestDbUpdateExceptionDestructurer() }))
+                .WriteTo.Sink(new TestTextWriterSink(jsonWriter, new JsonFormatter()))
+                .CreateLogger();
+            var ctx = new TestContext();
+            ctx.Database.EnsureDeleted();
+            ctx.Database.EnsureCreated();
+
+            var entry = ctx.Add(new User
+            {
+                UserId = Guid.NewGuid().ToString()
+            });
+
+            // Act
+            logger.Error(new TestDbUpdateException("DbUpdate Error", entry), "Error");
+
+            // Assert
+            var writtenJson = jsonWriter.ToString();
+            writtenJson.Should().NotContain(TestContext.UserIdIDoNotWantToSee);
+        }
+
+        public class User
+        {
+            public string UserId { get; set; }
+        }
+
+        public class TestContext : DbContext
+        {
+            public const string UserIdIDoNotWantToSee = "I Don't Want To See You";
+
+            public DbSet<User> Users { get; set; }
+
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseInMemoryDatabase(databaseName: "TestDebUpdateException");
+
+            protected override void OnModelCreating(ModelBuilder modelBuilder)
+            {
+                var users = new List<User>
+                {
+                    new User
+                    {
+                        UserId = "FirstUser"
+                    },
+                    new User
+                    {
+                        UserId = UserIdIDoNotWantToSee
+                    }
+                };
+
+                modelBuilder.Entity<User>().HasData(users);
+            }
+        }
+
+        internal class TestDbUpdateException : DbUpdateException
+        {
+            private readonly IReadOnlyList<EntityEntry> entityEntries;
+
+            public TestDbUpdateException(string message, EntityEntry entityEntry)
+                : base(message, (Exception)null) => this.entityEntries = new List<EntityEntry> { entityEntry }.AsReadOnly();
+
+            public override IReadOnlyList<EntityEntry> Entries => this.entityEntries;
+        }
+
+        internal class TestDbUpdateExceptionDestructurer : DbUpdateExceptionDestructurer
+        {
+            public override Type[] TargetTypes => new[] { typeof(TestDbUpdateException) };
+        }
+    }
+}
