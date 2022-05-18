f498b5e94f13547b1efd9aa6d2858d67b41b09f1
Muhammad Rehan Saeed <rehansaeed@gmail.com>
2022-05-18 15:58:53 +0100

    Use file scoped namespaces

--- Source/Serilog.Exceptions/Destructurers/RegexMatchTimeoutExceptionDestructurer.cs
+++ Source/Serilog.Exceptions/Destructurers/RegexMatchTimeoutExceptionDestructurer.cs
@@ -1,33 +1,31 @@
-namespace Serilog.Exceptions.Destructurers
+namespace Serilog.Exceptions.Destructurers;
+
+using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using Serilog.Exceptions.Core;
+
+/// <summary>
+/// Destructurer for <see cref="RegexMatchTimeoutException"/>.
+/// </summary>
+public class RegexMatchTimeoutExceptionDestructurer : ExceptionDestructurer
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Text.RegularExpressions;
-    using Serilog.Exceptions.Core;
-
-    /// <summary>
-    /// Destructurer for <see cref="RegexMatchTimeoutException"/>.
-    /// </summary>
-    public class RegexMatchTimeoutExceptionDestructurer
-        : ExceptionDestructurer
+    /// <inheritdoc cref="IExceptionDestructurer.TargetTypes"/>
+    public override Type[] TargetTypes => new Type[]
     {
-        /// <inheritdoc cref="IExceptionDestructurer.TargetTypes"/>
-        public override Type[] TargetTypes => new Type[]
-        {
             typeof(RegexMatchTimeoutException),
-        };
+    };
 
-        /// <inheritdoc cref="IExceptionDestructurer.Destructure"/>
-        public override void Destructure(Exception exception, IExceptionPropertiesBag propertiesBag, Func<Exception, IReadOnlyDictionary<string, object?>?> destructureException)
-        {
-            base.Destructure(exception, propertiesBag, destructureException);
+    /// <inheritdoc cref="IExceptionDestructurer.Destructure"/>
+    public override void Destructure(Exception exception, IExceptionPropertiesBag propertiesBag, Func<Exception, IReadOnlyDictionary<string, object?>?> destructureException)
+    {
+        base.Destructure(exception, propertiesBag, destructureException);
 
 #pragma warning disable CA1062 // Validate arguments of public methods
-            var typedException = (RegexMatchTimeoutException)exception;
-            propertiesBag.AddProperty(nameof(RegexMatchTimeoutException.Input), typedException.Input);
-            propertiesBag.AddProperty(nameof(RegexMatchTimeoutException.Pattern), typedException.Pattern);
-            propertiesBag.AddProperty(nameof(RegexMatchTimeoutException.MatchTimeout), typedException.MatchTimeout.ToString("c"));
+        var typedException = (RegexMatchTimeoutException)exception;
+        propertiesBag.AddProperty(nameof(RegexMatchTimeoutException.Input), typedException.Input);
+        propertiesBag.AddProperty(nameof(RegexMatchTimeoutException.Pattern), typedException.Pattern);
+        propertiesBag.AddProperty(nameof(RegexMatchTimeoutException.MatchTimeout), typedException.MatchTimeout.ToString("c"));
 #pragma warning restore CA1062 // Validate arguments of public methods
-        }
     }
 }
